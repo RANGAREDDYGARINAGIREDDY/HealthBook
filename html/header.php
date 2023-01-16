32fbc16267d41681a8aa8f794b2be41e5f6418e4
RANGAREDDY GARI NAGI REDDY <118525738+RANGAREDDYGARINAGIREDDY@users.noreply.github.com>
2023-01-16 21:05:36 +0530

    Create header.php

---
+++ html/header.php
@@ -0,0 +1,85 @@
+
+    <div class="row" style="background-color: white;">
+      <div class="col-md-1"></div>
+      <div class="col-md-10 col-sm-12 col-xs-12" >
+        <span style="float:right; padding-top:10px; font-size: smaller; color: gray;">
+          <img src="../images/flagFooter.png" style="width: 17px;">&nbsp;&nbsp;English &nbsp;&nbsp;&nbsp;&nbsp;
+            <img src="../images/watch.png" style="width: 16px;">&nbsp;&nbsp;<span id="idTime">00:00 - 24:00</span>&nbsp;&nbsp;&nbsp;&nbsp;
+          <img src="../images/phone1.png" style="width: 17px;">&nbsp;+91 8106975810/ +91 8106061168&nbsp;
+        </span>
+      </div>
+      <div class="col-md-1"></div>
+      <hr><br>
+    </div>
+
+     <nav class="navbar navbar-expand-lg navbar-light bg-info" style="margin-top: 5px;">
+  <a class="navbar-brand" href="../html/index.html" style="color: white;"><img src="../images/healthbook.ico" width="50" height="50">&nbsp;&nbsp;&nbsp;<b>HealthBook</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item active" >
+        <a class="nav-link" href="index.html">Home</a>
+      <li class="nav-item active" >
+        <a class="nav-link" href="stakeholder.html">Stake Holders</a>
+
+      <li class="nav-item">
+        <a class="nav-link" href="../help.html">Help</a>
+
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-dark" type="submit">Search</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a id="login-button" ms-hide-element="true" href="../logins/index.php" class="button logout login w-button">Login</a>
+
+<style>
+
+a#login-button.button.logout.login.w-button {
+  background-color: #66ffcc;
+  background-image: none;
+  border-radius: 25px;
+  border-width: 0;
+  bottom: auto;
+  box-sizing: border-box;
+  color: #253858;
+  cursor: pointer;
+  display: block;
+  flex: 1;
+  float: right;
+  font-family: Poppins, sans-serif;
+  font-size: 12px;
+  left: auto;
+  letter-spacing: 1px;
+  line-height: 19px;
+  margin-left: -15px;
+  margin-right: 15px;
+  margin-top: 15px;
+  padding: 5px 15px;
+  position: absolute;
+  right: 0;
+  text-align: center;
+  text-decoration: none;
+  top: 0;
+  transition: background-color 150ms;
+  z-index: 30;
+}
+ 
+a#login-button.button.logout.login.w-button:active {
+  outline: 0;
+}
+ 
+a#login-button.button.logout.login.w-button:hover {
+  background-color: #2ac28f;
+  font-weight: 400;
+  outline: 0;
+}
+ 
+</style>
+
+    </form>
+  </div>
+</nav>
