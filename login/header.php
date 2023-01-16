5a2776ccae132633045528f40dcb9c6ddc224a43
RANGAREDDY GARI NAGI REDDY <118525738+RANGAREDDYGARINAGIREDDY@users.noreply.github.com>
2023-01-16 19:51:10 +0530

    Add files via upload

---
+++ login/header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <!-- .....................................  Header codes start here  ................................................... -->
+
+    <?php
+    include ('../includes/header.php') ;
+  ?>
+</head>
+</html>
